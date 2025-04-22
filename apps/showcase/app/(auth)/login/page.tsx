98fdeb46d0559472bd7b7524d8d6b950c58cc0f8
Kamruzzaman <kzamanbn@gmail.com>
2025-04-22 19:20:23 +0600

    fix: login responsive issue

--- apps/showcase/app/(auth)/login/page.tsx
+++ apps/showcase/app/(auth)/login/page.tsx
@@ -16,7 +16,7 @@ const Login = async () => {
         redirect('/dashboard');
     }
     return (
-        <div className="flex min-h-screen items-center justify-center p-6">
+        <div className="flex min-h-screen items-center justify-center overflow-hidden p-6">
             <div className="login-card">
                 <div className="rounded-md bg-white px-8 py-6 shadow-md">
                     <Link href="/" className="my-4 flex items-center justify-center space-x-2">
@@ -26,7 +26,9 @@ const Login = async () => {
                         </p>
                     </Link>
                     <div className="my-3">
-                        <p className="mb-2 text-2xl font-semibold">Welcome to the adventure</p>
+                        <p className="mb-2 hidden text-2xl font-semibold md:block">
+                            Welcome to the adventure
+                        </p>
                         <p className="text-xs">
                             Please sign-in to your account and start the adventure
                         </p>
--- apps/showcase/components/shared/other-login-option.tsx
+++ apps/showcase/components/shared/other-login-option.tsx
@@ -1,9 +1,13 @@
 export default function OtherLoginOption() {
     return (
         <>
-            <div className="my-4 space-y-4 lg:flex lg:items-center lg:space-x-4 lg:space-y-0">
+            <div className="my-4 flex items-center gap-4">
                 <button className="text-default flex w-full items-center justify-center rounded-md border border-gray-200 bg-white p-2 text-sm hover:bg-gray-100 lg:w-1/2">
-                    <svg className="mr-2 h-5 w-5" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg
+                        className="mr-2 h-5 w-5"
+                        viewBox="0 0 21 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
                         <g clipPath="url(#clip0_13183_10121)">
                             <path
                                 d="M20.3081 10.2303C20.3081 9.55056 20.253 8.86711 20.1354 8.19836H10.7031V12.0492H16.1046C15.8804 13.2911 15.1602 14.3898 14.1057 15.0879V17.5866H17.3282C19.2205 15.8449 20.3081 13.2728 20.3081 10.2303Z"
@@ -20,15 +24,23 @@ export default function OtherLoginOption() {
                         </g>
                         <defs>
                             <clipPath id="clip0_13183_10121">
-                                <rect width="20" height="20" fill="white" transform="translate(0.5)"></rect>
+                                <rect
+                                    width="20"
+                                    height="20"
+                                    fill="white"
+                                    transform="translate(0.5)"></rect>
                             </clipPath>
                         </defs>
                     </svg>
-                    Sign in with Google
+                    <span className="hidden md:block">Sign in with Google</span>
                 </button>
 
                 <button className="text-default flex w-full items-center justify-center rounded-md border border-gray-200 bg-white p-2 text-sm hover:bg-gray-100 lg:w-1/2">
-                    <svg className="mr-2 h-5 w-5" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg
+                        className="mr-2 h-5 w-5"
+                        viewBox="0 0 21 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
                         <g clipPath="url(#clip0_13183_29163)">
                             <path
                                 d="M18.6574 15.5863C18.3549 16.2851 17.9969 16.9283 17.5821 17.5196C17.0167 18.3257 16.5537 18.8838 16.1969 19.1936C15.6439 19.7022 15.0513 19.9627 14.4168 19.9775C13.9612 19.9775 13.4119 19.8479 12.7724 19.585C12.1308 19.3232 11.5412 19.1936 11.0021 19.1936C10.4366 19.1936 9.83024 19.3232 9.18162 19.585C8.53201 19.8479 8.00869 19.985 7.60858 19.9985C7.00008 20.0245 6.39356 19.7566 5.78814 19.1936C5.40174 18.8566 4.91842 18.2788 4.33942 17.4603C3.71821 16.5863 3.20749 15.5727 2.80738 14.4172C2.37887 13.1691 2.16406 11.9605 2.16406 10.7904C2.16406 9.45009 2.45368 8.29407 3.03379 7.32534C3.4897 6.54721 4.09622 5.9334 4.85533 5.4828C5.61445 5.03219 6.43467 4.80257 7.31797 4.78788C7.80129 4.78788 8.4351 4.93738 9.22273 5.2312C10.0081 5.52601 10.5124 5.67551 10.7335 5.67551C10.8988 5.67551 11.4591 5.5007 12.4088 5.15219C13.3069 4.82899 14.0649 4.69517 14.6859 4.74788C16.3685 4.88368 17.6327 5.54699 18.4734 6.74202C16.9685 7.65384 16.2241 8.93097 16.2389 10.5693C16.2525 11.8454 16.7154 12.9074 17.6253 13.7506C18.0376 14.1419 18.4981 14.4444 19.0104 14.6592C18.8993 14.9814 18.7821 15.29 18.6574 15.5863V15.5863ZM14.7982 0.400358C14.7982 1.40059 14.4328 2.3345 13.7044 3.19892C12.8254 4.22654 11.7623 4.82035 10.6093 4.72665C10.5947 4.60665 10.5861 4.48036 10.5861 4.34765C10.5861 3.38743 11.0041 2.3598 11.7465 1.51958C12.1171 1.09416 12.5884 0.740434 13.16 0.458257C13.7304 0.18029 14.2698 0.0265683 14.7772 0.000244141C14.7921 0.133959 14.7982 0.267682 14.7982 0.400345V0.400358Z"
@@ -36,18 +48,22 @@ export default function OtherLoginOption() {
                         </g>
                         <defs>
                             <clipPath id="clip0_13183_29163">
-                                <rect width="20" height="20" fill="white" transform="translate(0.5)"></rect>
+                                <rect
+                                    width="20"
+                                    height="20"
+                                    fill="white"
+                                    transform="translate(0.5)"></rect>
                             </clipPath>
                         </defs>
                     </svg>
-                    Sign in with Apple
+                    <span className="hidden md:block">Sign in with Apple</span>
                 </button>
             </div>
 
             <div className="flex items-center justify-between">
-                <span className="w-1/2 border-b dark:border-dark"></span>
+                <span className="dark:border-dark w-1/2 border-b"></span>
                 <span className="mx-4 text-xs uppercase text-gray-500 dark:text-gray-400">or</span>
-                <span className="w-1/2 border-b dark:border-dark"></span>
+                <span className="dark:border-dark w-1/2 border-b"></span>
             </div>
         </>
     );
