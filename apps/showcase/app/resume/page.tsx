b9c223abd1e766ae703f57a2654f04299895e24e
Kamruzzaman <kzamanbn@gmail.com>
2025-04-28 20:16:59 +0600

    Adds YearsOfExperience component and utility function
    Implements a reusable component to calculate and display years of experience based on given dates.
    
    Removes redundant logic from the main page, improving code organization and readability.
    
    This change enhances maintainability and promotes code reuse.

--- apps/showcase/app/resume/page.tsx
+++ apps/showcase/app/resume/page.tsx
@@ -3,6 +3,7 @@ import CoverLetter from '@/components/resume/cover-letter';
 import TimelineIcon from '@/components/resume/timeline-icon';
 import Image from 'next/image';
 import { MdLocationPin } from 'react-icons/md';
+import YearsOfExperience, { getTotalYearsOfExperience } from './experience';
 
 import { profileImage, user } from '@/app/metadata';
 const social = {
@@ -16,55 +17,7 @@ export const metadata = {
     title: 'Resume | Kamruzzaman'
 };
 
-const YearsOfExperience = ({
-    startDate,
-    endDate
-}: {
-    startDate: string;
-    endDate?: string | null;
-}) => {
-    // get years of experience
-    const getYearsOfExperience = () => {
-        const date = new Date(startDate);
-        const currentDate = new Date(endDate || Date.now());
-        const diffInMs = currentDate.getTime() - date.getTime();
-
-        // Calculate total months
-        const totalMonths = Math.floor(diffInMs / (1000 * 60 * 60 * 24 * 30));
-
-        const years = Math.floor(totalMonths / 12);
-        const months = totalMonths % 12;
-
-        const yearPart = years > 0 ? `${years} yr${years > 1 ? 's' : ''}` : '';
-        const monthPart = months > 0 ? `${months} mo${months > 1 ? 's' : ''}` : '';
-
-        if (yearPart && monthPart) return `${yearPart} ${monthPart}`;
-        if (yearPart) return yearPart;
-        if (monthPart) return monthPart;
-        return null;
-    };
-
-    return <span className="text-primary-500 text-sm font-medium">({getYearsOfExperience()})</span>;
-};
-
 export default function Home() {
-    // get the total years of experience
-    const getTotalYearsOfExperience = () => {
-        const startDate = new Date('2020-05-01');
-        const currentDate = new Date();
-
-        let years = currentDate.getFullYear() - startDate.getFullYear();
-        let months = currentDate.getMonth() - startDate.getMonth();
-        const days = currentDate.getDate() - startDate.getDate();
-
-        if (months < 0 || (months === 0 && days < 0)) {
-            years--; // subtract a year if current month/day is before start month/day
-            months += 12;
-        }
-
-        const plusSign = months > 0 || days > 0 ? '+' : '';
-        return `${years}${plusSign} yr${years !== 1 ? 's' : ''}`;
-    };
     return (
         <div className="resume">
             <div className="page-container">
